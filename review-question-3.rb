629a28047178905a62ee799593f9b42a8b11075a
JingChen <jingchenjc2019@gmail.com>
2019-02-22 16:19:09 -0600

    review-question-3 completed. One to many relationships

--- review-question-3.rb
+++ review-question-3.rb
@@ -1,50 +1,67 @@
-# begin to build a simple program that models Instagram
-# you should have a User class, a Photo class and a comment class
+# User -> photo: 1 user--> many photos
+# Photo -> comment: 1 photo --> many comments
 
 class User
-    attr_accessor :name
+    attr_accessor :name, :photos
     def initialize(name)
         @name = name
-    end
-
-    def photos
-        Photo.new
+        @photos = Photo.new
     end
 end
 
 class Photo
-    def user
-        User(name)
+    @@all = []
+    attr_accessor :user
+    def initialize
+      @@all << self
+    end
+
+    def comments
+    #below: go through all comments, select the commets that belong to the photo.
+       Comment.all.select do | comment |
+          comment.photo == self #select according to this condition
+       end
+    end
+
+    def make_comment(comment)
+      Comment.new(self)
+    end
+
+    def self.all
+      @@all
     end
 
 end
 
-class Comments
+class Comment
+
     @@all = []
-    def initialize
-        @comments = []
+    attr_accessor :photo
+    def initialize(photo)
+      @photo = photo
+      @@all << self
     end
     
+    def self.all
+      @@all
+    end
 end
 
 
-sandwich_photo = Photo.new
+sandwich_photo = Photo.new #create a new Photo instance, no property initialized
 
-sophie = User.new("Sophie")
+sophie = User.new("Sophie") #create a new User instance, one property
+sandwich_photo.user = sophie #Photo.new.user, photo has a user property
+sandwich_photo.user.name  #Photo.new.user.name, photo has a user property, user directs to name property
+# => "Sophie"  PASSED
 
-sandwich_photo.user = sophie
+sophie.photos #User.new("Sophie").photos, user has a photos property
+# => [#<Photo:0x00007fae2880b370>] PASSED
 
-sandwich_photo.user.name
-# => "Sophie"
+sandwich_photo.comments  #Photo.new.comments
+# => [] #Passed
 
-
-sophie.photos
-# => [#<Photo:0x00007fae2880b370>]
-
-sandwich_photo.comments
-# => []
-
-sandwich_photo.make_comment("this is such a beautiful photo of your lunch!! I love photos of other people's lunch")
+sandwich_photo.make_comment("this is such a beautiful photo of your lunch!! I love photos of other people's lunch") #Photo.new.make_comment("string")
 
 sandwich_photo.comments
 # => [#<Comment:0x00007fae28043700>]
