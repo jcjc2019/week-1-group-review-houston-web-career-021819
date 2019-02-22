b42da60ef5a4c88c10e5314278cd815dd0aafa05
JingChen <jingchenjc2019@gmail.com>
2019-02-22 13:08:13 -0600

    review-question-2 completed

--- review-question-2.rb
+++ review-question-2.rb
@@ -1,12 +1,17 @@
 # Finish the implementation of the Car class so it has the functionality described below
 
 class Car
+
   @@all = []
   attr_accessor :make, :model
   def initialize(make, model)
     @make = make
     @model = model
-    @drive = "VROOOOOOOOOOOOM!"
+    @@all << self
+  end
+
+  def drive
+    "VROOOOOOOOOOOOM!"
   end
 
   def self.all
@@ -34,6 +39,23 @@ Car.all
 #=> [#<Car:0x00007fae28930f20>, #<Car:0x00007fae28923370>, #<Car:0x00007fae2891ae78>]
 
 BONUS:
+
+#in order to make the line below work, we have to change the previous class code into the following code:
+
+class Car
+  
+  attr_accessor :make, :model
+  #to initialize an empty hash, with 2 key-value pairs
+  def initialize(params ={})
+    @make = params.fetch(:make, make)
+    @model = params.fetch(:model, model)
+  end
+
+  def to_s #change the key-value pair to string
+    "#{make} #{model}"
+  end
+
+end
 
 volvo_lightning = Car.new(make: "Volvo", model: "Lightning")
 
